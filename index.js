c2da6742c9b2c2fc3292d6e24b506ac5667c343d
Cameron Chandler <webstormcam@gmail.com>
2021-05-28 10:15:43 -0400

    Increment function now works!

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-//document.getElementById('count-el').innerText=5
 
+let count=0
 function increment(){
-    console.log('The button was clicked')
+    count+=1
+    document.getElementById('count-el').innerText=count
 }
